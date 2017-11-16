58168c3380bf79a627f2cc2e4ec4e3bee9eb9253
Msluka <969179@gmail.com>
2017-11-16 23:50:02 +0100

    Initial Commit

---
+++ B2B/B2B/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(B2B.Startup))]
+namespace B2B
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
